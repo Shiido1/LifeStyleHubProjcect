ed387c3c5a6684558f3b0dcddc3ade1ae5a5dcbc
JayCode <vsquare396@gmail.com>
2021-11-06 15:15:25 +0100

    Social media share doesn’t work

--- lib/ui/screens/dashboard/fragments/marketting/marketting_details_screen.dart
+++ lib/ui/screens/dashboard/fragments/marketting/marketting_details_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive/hive.dart';
 import 'package:lifestyle_hub/helper/configs/instances.dart';
+import 'package:share/share.dart';
 import '../profile/dao/profile_dao.dart';
 import '../profile/model/users_profile_model.dart';
 import '../../../../../helper/helper_handler.dart';
@@ -144,89 +145,24 @@ class _MarkettingDetailScreenState extends State<MarkettingDetailScreen> {
                     ),
                   ),
             SizedBox(height: 24),
-            TextView(
-              text: getResourcesModel!.title!,
-              fontWeight: FontWeight.w700,
-              fontSize: 16,
-              color: Pallets.grey600,
-              textAlign: TextAlign.left,
+            Row(
+              children: [
+                Expanded(
+                  child: TextView(
+                    text: getResourcesModel!.title!,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                    color: Pallets.grey600,
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                IconButton(
+                    onPressed: () =>
+                        Share.share(getResourcesModel?.content?.path ?? ''),
+                    icon: const Icon(Icons.share)),
+              ],
             ),
             SizedBox(height: 23),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.start,
-            //   mainAxisSize: MainAxisSize.max,
-            //   children: [
-            //     Expanded(
-            //       flex: 2,
-            //       child: Row(
-            //         mainAxisSize: MainAxisSize.min,
-            //         crossAxisAlignment: CrossAxisAlignment.center,
-            //         children: [
-            //           Icon(
-            //             CupertinoIcons.eye,
-            //             color: Pallets.grey400,
-            //           ),
-            //           SizedBox(width: 8),
-            //           TextView(
-            //             text: '1.2K views',
-            //             fontWeight: FontWeight.w500,
-            //             fontSize: 12,
-            //             color: Pallets.grey400,
-            //             textAlign: TextAlign.left,
-            //           ),
-            //         ],
-            //       ),
-            //     ),
-            //     Expanded(
-            //       flex: 3,
-            //       child: Row(
-            //         mainAxisSize: MainAxisSize.min,
-            //         crossAxisAlignment: CrossAxisAlignment.center,
-            //         children: [
-            //           Icon(
-            //             CupertinoIcons.share,
-            //             color: Pallets.grey400,
-            //           ),
-            //           SizedBox(width: 8),
-            //           TextView(
-            //             text: '400 shares',
-            //             fontWeight: FontWeight.w500,
-            //             fontSize: 12,
-            //             color: Pallets.grey400,
-            //             textAlign: TextAlign.left,
-            //           ),
-            //         ],
-            //       ),
-            //     )
-            //   ],
-            // ),
-            // SizedBox(height: 24),
-            // TextView(
-            //   text: 'Share this',
-            //   fontWeight: FontWeight.w700,
-            //   fontSize: 16,
-            //   color: Pallets.grey800,
-            //   textAlign: TextAlign.left,
-            // ),
-            // SizedBox(height: 10),
-            // SingleChildScrollView(
-            //   scrollDirection: Axis.horizontal,
-            //   child: Row(
-            //     children: SocialMediaModel.getSocialMedia()
-            //         .map((social) => Container(
-            //             height: 60,
-            //             width: 60,
-            //             margin: EdgeInsets.only(right: 16.5),
-            //             decoration: BoxDecoration(
-            //                 shape: BoxShape.circle, color: social.color),
-            //             child: ImageLoader(
-            //               path: social.media!,
-            //             )))
-            //         .toList(),
-            //   ),
-            // ),
-            // SizedBox(height: 30),
-
             ValueListenableBuilder(
                 valueListenable: markettingDao!.getListenable()!,
                 builder: (_, Box<dynamic> box, __) {
